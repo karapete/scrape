51fd1608b13e685f261b09341d6e04c8823c3f79
Kara Peterson <karapeterson@karas-macbook.uconnect.utah.edu>
2017-10-25 18:25:36 -0600

    2nd commit

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
-server.js
+//dependencies
+
+var express = require("express");
+var bodyParser = require("body-parser")
+var mongoose = require("mongoose");
+
+//these dependencies make it possible to scrape
+
+var request = ("request");
+var cheerio = ("cheerio");
+
+//initialize express
+var app express();
+
+var databaseUrl = "scraper";
+var 
